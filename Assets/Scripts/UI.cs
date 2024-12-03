e5453f8bafaf7579bde314c892fc83970d3c695a
Анна <96340630+Ayn6@users.noreply.github.com>
2024-12-03 17:16:57 +0400

    Menu

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI : MonoBehaviour
+{
+    public void LoadGame() {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Exit() {
+        Application.Quit();
+        Debug.Log("Exit");
+    }
+}
