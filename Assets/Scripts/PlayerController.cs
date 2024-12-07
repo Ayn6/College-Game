c89d6954e110dd6bcd98117cb49b74e5db7703f1
voidestt <matveylazzz@gmail.com>
2024-12-07 13:29:11 +0400

    Стамина и ГГ
    сделал систему стамины и добавил ГГ

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,27 +8,35 @@ public class PlayerController : MonoBehaviour
     public float runSpeed;
     private float nowSpeed; 
     private Vector2 moveVector;
-
+    public float stamina = 5;
+   
     void Start()
     {
         nowSpeed = walkSpeed;
+       
 
     }
-
     private void FixedUpdate()
     {
         moveVector.x = Input.GetAxis("Horizontal");
         moveVector.y = Input.GetAxis("Vertical");
         rb.MovePosition(rb.position + moveVector * nowSpeed * Time.deltaTime);
 
-        if(Input.GetKey(KeyCode.LeftShift))
+        if(Input.GetKey(KeyCode.LeftShift) && stamina > 0)
         {
+            stamina -= Time.deltaTime;
             nowSpeed = runSpeed;
             
         }
-        else
+        if(!(Input.GetKey(KeyCode.LeftShift) && stamina > 0))
         {
+            stamina += Time.deltaTime;
             nowSpeed = walkSpeed;
         }
+        if (stamina >= 5) stamina = 5;
+        if (stamina <= 0) stamina = 0;
+        
+        
     }
+
 }
